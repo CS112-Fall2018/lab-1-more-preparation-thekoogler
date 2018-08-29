01b1e36629437282dd600e73907f530b1ac5fb43
thekoogler <42752213+thekoogler@users.noreply.github.com>
2018-08-29 16:33:12 -0700

    Add files via upload

---
+++ arithmetic.java
@@ -0,0 +1,44 @@
+// Creates 4 local variables
+
+// Performs the following arithmetic operations on every combination of two of these 
+// and output using System.out.println each of the results
+
+import java.util.Scanner;
+
+class AddNumbers
+{
+	public static void main(String args[])
+	{
+
+		int a = 4;
+		int b = 6;
+		float x = 12.5;
+		float y = 11.2;
+
+		System.out.println(a + b);
+		System.out.println(a + x);
+		System.out.println(a + y);
+		System.out.println
+		System.out.println
+		System.out.println
+		System.out.println
+		System.out.println
+		System.out.println
+a + y;
+b + x;
+b + y;
+x + y;
+
+a / b;
+a / x;
+a / y;
+b / x;
+b / y;
+x / y;
+
+b / a;
+x / a;
+y / a;
+x / b;
+y / b;
+y / x;
